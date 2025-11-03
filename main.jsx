d2deba0255216e41f78afcfd45cc342a2dfbe8eb
unknown <marg.ec256@gmail.com>
2025-11-03 09:46:53 -0300

    Move main to root folder

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext.jsx'
+import App from './src/App.jsx'
+import { AuthProvider } from './src/context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
